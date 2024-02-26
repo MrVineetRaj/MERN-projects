6e2478698004a81be0fd9c965b95eb47d8ebe082
Vineet Raj <112626546+MrVineetRaj@users.noreply.github.com>
2024-02-26 07:27:47 +0530

    Add files via upload

---
+++ biteCounter/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+int main()
+{
+  int s = 4;
+  int b = 1;
+  cout << (s - b) << endl;
+}
